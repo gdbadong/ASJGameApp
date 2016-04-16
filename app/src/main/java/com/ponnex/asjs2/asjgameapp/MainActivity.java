563070acfd419fa6aab33c018f0bc31b40c9ba93
ponnex <ponnex@gmail.com>
2016-04-17 01:35:15 +0800

    Initial commit

---
+++ app/src/main/java/com/ponnex/asjs2/asjgameapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ponnex.asjs2.asjgameapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.image);
+    }
+
+    public void onClick(View view) {
+        imageView.setVisibility(View.INVISIBLE);
+    }
+}
